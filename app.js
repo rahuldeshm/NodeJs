da0fc7c3bf7106cb90c9cb895fb4053a45aff4f5
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-08 12:13:36 +0530

    cleaned the app.js file and added the logic in routes file and then exported it and imported it in app.js file.

--- app.js
+++ app.js
@@ -1,54 +1,13 @@
 //Creating a Node Server.
-
+const routes = require("./routes");
 const http = require("http");
-const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  // const server = http.createServer(routes) can do like this to
+  routes.handler(req, res);
   // console.log("Rahul");
   //process.exit(); to exit the registered event listener.
   //hard exit our loop so our project shut down.
-
-  if (req.url === "/") {
-    fs.readFile(
-      "C:\\Users\\RAHUL\\Desktop\\Node.js\\Hello.text",
-      "utf8",
-      (e, d) => {
-        res.write("<html>");
-        res.write("<head> <title> enter message</title></head>");
-        res.write(
-          `<body><p>${d}</p><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>submit</button></form></body>`
-        );
-        res.write("</html>");
-        return res.end();
-      }
-    );
-  } // if (req.url === "/node") {
-  //   res.write("<body><h1>Welcome to my node.js project</body></h1>");
-  // } else if (req.url === "/about") {
-  //   res.write("<body><h1>Welcome to about page .</body></h1>");}
-  else if (req.url === "/message" && req.method === "POST") {
-    const body = [];
-    req.on("data", (e) => {
-      console.log(e);
-      body.push(e);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("hello.text", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      }); //this is sync file and this block the stack
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head> <title> My first backend</title></head>");
-    res.write("<body><h1>form submitted</body></h1>");
-    res.write("</html>");
-    res.end();
-  }
 });
 
-server.listen(4000);
+server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+function requestHandler(req, res) {
+  if (req.url === "/") {
+    fs.readFile(
+      "C:\\Users\\RAHUL\\Desktop\\Node.js\\Hello.text",
+      "utf8",
+      (e, d) => {
+        res.write("<html>");
+        res.write("<head> <title> enter message</title></head>");
+        res.write(
+          `<body><p>${d}</p><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>submit</button></form></body>`
+        );
+        res.write("</html>");
+        return res.end();
+      }
+    );
+  } // if (req.url === "/node") {
+  //   res.write("<body><h1>Welcome to my node.js project</body></h1>");
+  // } else if (req.url === "/about") {
+  //   res.write("<body><h1>Welcome to about page .</body></h1>");}
+  else if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (e) => {
+      console.log(e);
+      body.push(e);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("hello.text", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      }); //this is sync file and this block the stack
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head> <title> My first backend</title></head>");
+    res.write("<body><h1>form submitted</body></h1>");
+    res.write("</html>");
+    res.end();
+  }
+}
+
+// module.exports = requestHandler;
+module.exports = {
+  handler: requestHandler,
+  someText: "somehard coded text",
+};
+
+// module.exports.handler = requestHandler  // different way to export
+// module.exports.someText = "some testlkfsda"
+
+// exports.handler = requestHandler// another way to export.
+// exports.someText = "some testlkfsda"
