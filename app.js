feb53d36f4d0bcc499835dfb5ba2eddf6f8fe526
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-08 12:50:58 +0530

    Added package.json file and added nodemon as a dependency and also added such that when npm start it will start our code but refreshes the code when updated

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 const routes = require("./routes");
 const http = require("http");
 
+console.log(routes.someText);
 const server = http.createServer((req, res) => {
   // const server = http.createServer(routes) can do like this to
   routes.handler(req, res);
-  // console.log("Rahul");
+  console.log("Rahul");
   //process.exit(); to exit the registered event listener.
   //hard exit our loop so our project shut down.
 });
