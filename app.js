fddfb1f6807e5d25db6b6de037b64a1f08b68c37
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-08 08:27:41 +0530

    Added different routes so will give different response for different requests.

--- app.js
+++ app.js
@@ -3,7 +3,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Rahul");
+  // console.log("Rahul");
+  //process.exit(); to exit the registered event listener.
+  //hard exit our loop so our project shut down.
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head> <title> My first backend</title></head>");
+  if (req.url === "/node") {
+    res.write("<body><h1>Welcome to my node.js project</body></h1>");
+  } else if (req.url === "/about") {
+    res.write("<body><h1>Welcome to about page .</body></h1>");
+  } else {
+    res.write("<body><h1>welcome to home page of our project</body></h1>");
+  }
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(4000);
