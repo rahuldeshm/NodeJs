c00a219aaf3114ddfba8e59cc941ff7b4253a643
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-06-10 15:41:25 +0530

    1. moved all the html code into view folder. 2. added page not found with response code 404 for url doesn't exist. 3. used path helper 4. added styling statically to html pages. 5. added contactus page.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // const http = require("http");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
@@ -7,12 +8,17 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use("/shop", shopRoutes);
-
+app.use("/contactus", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "contactus.html"));
+});
+app.use("/success", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "success.html"));
+});
 app.use("/", (req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "notFound.html"));
 });
 app.listen(3000);
 // const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("./../util/path");
 
 const router = express.Router();
 
 router.get("/addproduct", (req, res, next) => {
   console.log("inProduct");
-  res.send(
-    "<form action='/admin/addproduct' method='POST' ><input  type='text' name='title'  /><br><input  type='text' name='size'  /><br><button type='submit'>submit</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "views", "addProduct.html"));
 });
 router.post("/addproduct", (req, res, next) => {
   console.log(req.body);
